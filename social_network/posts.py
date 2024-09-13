3e3d63a04d762c466513c01ff22d99819beeb5df
Basil Alrashedi <121049280+Zeen1th@users.noreply.github.com>
2024-09-13 17:22:09 +0300

    Update posts.py

--- social_network/posts.py
+++ social_network/posts.py
@@ -1,36 +1,66 @@
 from datetime import datetime
 
-# Please remove the comments and 
-# create these classes as it corresponds:
-# (your tests will fail if you don't comment out these classes)
+class Post:
+    def __init__(self, text, timestamp=None):
+        self.text = text
+        self.user = None  # User is initially None
+        self.timestamp = timestamp or datetime.now()
 
-# class Post(object):
-#     def __init__(self, text, timestamp=None):
-#         pass
-#
-#     def set_user(self, user):
-#         pass
+    def set_user(self, user):
+        self.user = user
 
+class TextPost(Post):
+    def __init__(self, text, timestamp=None):
+        super().__init__(text, timestamp)
 
-# class TextPost(...):  # Inherit properly
-#     def __init__(self, text, timestamp=None):
-#         pass
-#
-#     def __str__(self):
-#         pass
+    def __str__(self):
+        user_str = f"@{self.user.first_name} {self.user.last_name}" if self.user else "Unknown User"
+        timestamp_str = self.timestamp.strftime('%A, %b %d, %Y')
+        return f'{user_str}: "{self.text}"\n\t{timestamp_str}'
 
+class PicturePost(Post):
+    def __init__(self, text, image_url, timestamp=None):
+        super().__init__(text, timestamp)
+        self.image_url = image_url
 
-# class PicturePost(...):  # Inherit properly
-#     def __init__(self, text, image_url, timestamp=None):
-#         pass
-#
-#     def __str__(self):
-#         pass
+    def __str__(self):
+        user_str = f"@{self.user.first_name} {self.user.last_name}" if self.user else "Unknown User"
+        timestamp_str = self.timestamp.strftime('%A, %b %d, %Y')
+        return f'{user_str}: "{self.text}"\n\t{self.image_url}\n\t{timestamp_str}'
 
+class CheckInPost(Post):
+    def __init__(self, text, latitude, longitude, timestamp=None):
+        super().__init__(text, timestamp)
+        self.latitude = latitude
+        self.longitude = longitude
 
-# class CheckInPost(...):  # Inherit properly
-#     def __init__(self, text, latitude, longitude, timestamp=None):
-#         pass
-#
-#     def __str__(self):
-#         pass
+    def __str__(self):
+        user_str = f"@{self.user.first_name} Checked In" if self.user else "Unknown User"
+        timestamp_str = self.timestamp.strftime('%A, %b %d, %Y')
+        return f'{user_str}: "{self.text}"\n\t{self.latitude}, {self.longitude}\n\t{timestamp_str}'
+
+class User:
+    def __init__(self, first_name, last_name, email):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.email = email
+        self.posts = []
+        self.following = []
+
+    def add_post(self, post):
+        post.set_user(self)  # Set the post's user to this user
+        self.posts.append(post)
+
+    def follow(self, other):
+        self.following.append(other)
+
+    def get_timeline(self):
+        # Get all posts from followed users
+        timeline_posts = []
+        for followed_user in self.following:
+            timeline_posts.extend(followed_user.posts)
+        # Sort by timestamp (newest first)
+        return sorted(timeline_posts, key=lambda post: post.timestamp, reverse=True)
+
+    def __str__(self):
+        return f'<User: "{self.first_name} {self.last_name}">'
